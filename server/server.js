5668dd2e1777335b918fb726412e34400e1b43c8
sahil <emailtosahil@yahoo.com>
2018-09-24 01:32:07 +0530

    First Commit

--- server/server.js
+++ server/server.js
@@ -4,9 +4,9 @@ const express=require('express');
 const app=express();
 
 const publicPath=path.join(__dirname,'../public');
-
+const port=process.env.port || 3000;
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>{
-    console.log("App is running on port: 3000");
+app.listen(port,()=>{
+    console.log("App is running on port: "+port);
 });
