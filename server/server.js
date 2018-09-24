2c9c382ebced6cac50630b61b1a9563f256a582f
sahil <emailtosahil@yahoo.com>
2018-09-24 09:27:32 +0530

    Third Commit

--- server/server.js
+++ server/server.js
@@ -4,9 +4,10 @@ const express=require('express');
 const app=express();
 
 const publicPath=path.join(__dirname,'../public');
+
 const port=process.env.Port || 3000;
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
-    console.log("App is running on port: "+port);
+app.listen(port, () => {
+    console.log(`Server is up on ${port}`);
 });
