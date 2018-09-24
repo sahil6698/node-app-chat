9c14473252dca48b33a78993fdbfcf2de3f088e1
sahil <emailtosahil@yahoo.com>
2018-09-24 09:07:20 +0530

    Fix Port Issue

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express=require('express');
 const app=express();
 
 const publicPath=path.join(__dirname,'../public');
-const port=process.env.port || 3000;
+const port=process.env.Port || 3000;
 app.use(express.static(publicPath));
 
 app.listen(port,()=>{
